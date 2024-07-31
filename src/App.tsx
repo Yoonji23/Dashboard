955faa84344558523cf6be037bd66f31dbde7dc6
djwjfk <grw07236@naver.com>
2024-07-31 18:25:32 +0900

    Build: firebase에 json데이터 연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { DashboardLayout } from "./layout/DashboardLayout";
 import { Main } from "./page/Main";
+import { db } from "./firebase";
+
+import { collection, query, getDocs } from "firebase/firestore";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const getCook = async () => {
+      const q = query(collection(db, "cook"));
+
+      const querySnapshot = await getDocs(q);
+
+      querySnapshot.forEach((doc) => {
+        console.log(doc.data());
+      });
+    };
+    getCook();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
