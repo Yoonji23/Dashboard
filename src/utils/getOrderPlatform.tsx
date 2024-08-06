cd4bee5acbc19c622a823e10e9fa6fa6dc60bb86
djwjfk <grw07236@naver.com>
2024-08-06 22:16:55 +0900

    fix: 정렬 기준 내림차순

--- src/utils/getOrderPlatform.tsx
+++ src/utils/getOrderPlatform.tsx
@@ -7,5 +7,18 @@ export async function getOrderPlatform(data: any[]) {
     return acc;
   }, {});
 
-  return result;
+  // 객체를 배열로 변환
+  const orderPlatformArray = Object.entries(result);
+
+  // 배열을 값 기준으로 내림차순 정렬
+  const sortedEntries = orderPlatformArray.sort(([, valueA], [, valueB]) => {
+    const numValueA = Number(valueA);
+    const numValueB = Number(valueB);
+    return numValueB - numValueA;
+  });
+
+  // 정렬된 배열을 다시 객체로 변환
+  const sortedResult = Object.fromEntries(sortedEntries);
+
+  return sortedResult;
 }
